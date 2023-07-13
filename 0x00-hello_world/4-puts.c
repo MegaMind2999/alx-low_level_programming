85f0af8dcbbae4d96e7459bc07f6f4f911b4fe7d
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-07-13 13:12:10 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-prinft("\"Programming is like building a multilingual puzzle\n");
+printf("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
