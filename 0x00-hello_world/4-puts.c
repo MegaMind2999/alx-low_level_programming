fefa5ea5c73fe265c7f6f25a9b18b383b49c4c0e
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-07-13 13:16:06 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - just a function that prints using puts
+ * Return: 0
+ */
 int main(void)
 {
-printf("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
