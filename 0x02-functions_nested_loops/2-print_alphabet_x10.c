cc8ca4010e2680804245d76d42fa834ac9d64d18
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-07-18 13:49:21 -0700

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,9 +5,11 @@
 */
 void print_alphabet_x10(void)
 {
-for (int i = 0; i < 10; i++)
+int i = 0;
+char a = 'a';
+for (i = 0; i < 10; i++)
 {
-for (char a = 'a'; a <= 'z'; a++)
+for (a = 'a'; a <= 'z'; a++)
 _putchar(a);
 _putchar(10);
 }
