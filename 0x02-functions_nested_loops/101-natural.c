32bff5d341db5ef08f8b7e854dd8f4f493ddcd55
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-07-18 16:33:01 -0700

    new

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - count the multiples
+*of 3 or 5 below 1024
+*Return: nothing
+*/
+void main(void)
+{
+int n = 0;
+int i = 0;
+for (i = 3; i < 1024; i += 3)
+n += i;
+
+for (i = 5; i < 1024; i += 5)
+n += i;
+
+printf("%d", n);
+}
