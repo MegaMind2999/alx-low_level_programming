ecd7b2d5e48550f4a227b82d0f698fe1c5f56828
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-07-20 12:56:49 -0700

    new

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+*main - fizz or buzz or fizzBuzz
+*
+*
+*Return: zero
+*/
+int main(void)
+{
+int n;
+while (n++ < 100)
+
+if ((n % 3 == 0) && (n % 5 == 0))
+printf("FizzBuzz ");
+
+else if ((n % 3) == 0)
+printf("Fizz ");
+
+else if ((n % 5) == 0)
+{
+if (n != 100)
+printf("Buzz ");
+
+else
+printf("Buzz");
+}
+
+else
+printf("%d ", n);
+
+printf("\n");
+return (0);
+}
