c6ab418e2eb0d2c6d770b70822a6a193b15607fb
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-07-20 12:43:01 -0700

    new

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_line - printing straight line
+*@n: count of _
+*Return:returns nothing
+*/
+void print_line(int n)
+{
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
