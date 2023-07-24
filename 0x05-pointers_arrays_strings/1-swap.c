75483764a909e57703dbfdb9eba3bea1dcf89904
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-07-24 14:49:56 -0700

    new

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ * @a: pointer to first int
+ * @b: pointer to second int
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
