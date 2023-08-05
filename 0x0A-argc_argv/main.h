3cc1a86ed129ab6c1729d02c3b3de8ae0496e7ad
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-08-04 17:44:32 -0700

    new

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,12 +2,5 @@
 #define MAIN_H
 
 int _putchar(char c);
-char *create_array(unsigned int size, char c);
-void free_grid(int **grid, int height);
-char *str_concat(char *s1, char *s2);
-char *_strdup(char *str);
-int **alloc_grid(int width, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
 
-# endif
+#endif
