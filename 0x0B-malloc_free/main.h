ee7c6789f47d712bfc31aadf4029e27510d90d99
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-08-07 22:12:52 -0700

    new

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-char *argstostr(int ac, char **av);
-char *str_concat(char *s1, char *s2);
+
 char *create_array(unsigned int size, char c);
-int _putchar(char c);
-char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+char *argstostr(int ac, char **av);
 int **alloc_grid(int width, int height);
+char *_strdup(char *str);
 void free_grid(int **grid, int height);
+int _putchar(char c);
+
 #endif
