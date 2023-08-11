45feee7cc1b70b37899f05aa00751148c8492f8c
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-08-10 17:23:24 -0700

    new

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define min(x, y) (((x) < (y)) ? (x) : (y))
-
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
