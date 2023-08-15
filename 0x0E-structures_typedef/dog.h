0425fdbbc3412f4cbaf18f1985e4a8b61f4785ab
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-08-14 17:35:01 -0700

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,11 @@ char *name;
 float age;
 char *owner;
 } dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+char *_strncpy(char *dest, char *src, int n);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
 #endif
