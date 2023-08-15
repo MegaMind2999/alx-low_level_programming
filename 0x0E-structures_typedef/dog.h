fa2e73886aea13790f113f25fc2f6863333fb1b1
MegaMind2999 <abdo.mohamed29999@gmail.com>
2023-08-14 17:31:53 -0700

    new

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - the dog struct
+ * @name: name
+ * @age: age
+ * @owner: the owner
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+#endif
